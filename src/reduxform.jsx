f2669ce7e7a9280f49934b1f526feabab5904922
ffiargus <rm.chow@outlook.com>
2017-07-19 01:27:19 -0400

    adds validations for form values

--- src/reduxform.jsx
+++ src/reduxform.jsx
@@ -3,19 +3,46 @@ import { Field, reduxForm } from 'redux-form';
 
 const validate = values => {
   const errors = {}
-  if (!values.username) {
-    errors.username = 'Required'
+  if (!values.title) {
+    errors.title = 'Required'
+  }
+  // if (!values.data) {
+  //   errors.data = 'Required'
+  // }
+  if (!values.icon) {
+    errors.icon = 'Required'
+  }
+  if (!values.type) {
+    errors.type = 'Required'
+  }
+  if (!values.serviceid) {
+    errors.serviceid = 'Required'
   }
   return errors
 }
 
-const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+const renderField = ({ input, label, type, meta: { touched, error} }) => (
   <div>
     <label>{label}</label>
     <div>
       <input {...input} placeholder={label} type={type}/>
-      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+      {touched && ((error && <span>{error}</span>))}
     </div>
+  </div>
+)
+
+const renderSelectField = ({ input, label, type, meta: { touched, error} }) => (
+  <div>
+    <label>{label}</label>
+    <Field name="type" component="select">
+      <option />
+      <option >Meetup</option>
+      <option >Entertainment</option>
+      <option >Lunch & Learn</option>
+      <option >Party</option>
+      <option >Other</option>
+    </Field>
+    {touched && ((error && <span>{error}</span>))}
   </div>
 )
 
@@ -26,14 +53,8 @@ const SyncValidationForm = (props) => {
       <Field name="title" type="text" component={renderField} label="Title"/>
       <Field name="data" component="textarea" label="Description"/>
       <Field name="icon" type="text" component={renderField} label="Icon URL"/>
-      <Field name="type" component="select" label="Username">
-        <option />
-        <option >Meetup</option>
-        <option >Entertainment</option>
-        <option >Lunch & Learn</option>
-        <option >Party</option>
-        <option >Other</option>
-      </Field>
+      <Field name="type" component={renderSelectField} label="Type" />
+
       <Field name="serviceid" type="text" component={renderField} label="Service ID"/>
       <div>
         <button type="submit" disabled={submitting}>Submit</button>

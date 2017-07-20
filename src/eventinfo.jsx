761760ae2d8206fe94ff4b9d1cf05ef17b9d7247
ffiargus <rm.chow@outlook.com>
2017-07-20 00:23:07 -0400

    displays all event information in sidebar, minor styling to sidebar

--- src/eventinfo.jsx
+++ src/eventinfo.jsx
@@ -21,6 +21,7 @@ export default class EventInfo extends Component {
   //else display event info
   render() {
     const { event } = this.props;
+    // const postDate = new Date(parseInt(event.timestamp, 10));
     return (
       <div className="event-info">
         {
@@ -28,10 +29,15 @@ export default class EventInfo extends Component {
           ? <div className="event-details">
               <img src={event.icon} width="150" height="150"/>
               <h1>{event.title}</h1>
+              <h2>{event.type}</h2>
               <p>{event.data}</p>
-              <button onClick={this._deleteEvent}>
+              <button className="delete-btn" onClick={this._deleteEvent}>
                 Delete
               </button>
+              <div className="tags">
+                <p className="id-tag">Service ID: {event.serviceid}</p>
+                <p className="date-tag">Posted: {new Date(parseInt(event.timestamp, 10)).toString()}</p>
+              </div>
             </div>
           : <p>No event Selected</p>
         }

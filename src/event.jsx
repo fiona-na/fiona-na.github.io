19b1a8430f772dd486e83fdc72b011a1d91f62f6
ffiargus <rm.chow@outlook.com>
2017-07-20 01:29:35 -0400

    adds hover style to events and keeps track of most recently selected event

--- src/event.jsx
+++ src/event.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 //simple component, just make the individual event boxes
+//takes in selected events id, and adds additional class
+//to component if it is equal to self's id
 export default class Event extends Component {
   render() {
-    const { type, serviceid, icon, timestamp, title, data } = this.props.event;
+    const { type, serviceid, icon, timestamp, title, data, id } = this.props.event;
     return (
-      <div className="event-box" onClick={() => {this.props.handleClick(this.props.event)}}>
+      <div
+        className={this.props.selected === id ? "event-box selected-event" : "event-box"}
+        onClick={() => {this.props.handleClick(this.props.event)}}
+      >
         <img src={icon} height="70" width="70"/>
         <div>
           <h2>{title}</h2>
--- src/eventcontainer.jsx
+++ src/eventcontainer.jsx
@@ -3,16 +3,19 @@ import Event from './event.jsx';
 import { connect } from 'react-redux';
 
 //Grab events and loading info from store
+//grab selected event as well to tell event
+//component which one was last clickked
 @connect((state) => {
   return {
     events: state.event.events,
+    selectedEvent: state.event.selectedEvent,
     loading: state.event.loading
   };
 })
 
 export default class EventContainer extends Component {
   render() {
-    const { events } = this.props;
+    const { events, selectedEvent } = this.props;
 
     //check if events exists if it does,
     //map through each one and print it,
@@ -24,7 +27,12 @@ export default class EventContainer extends Component {
           ? <div>
             {
               events.map((event) =>
-                <Event event={event} key={event.id} handleClick={this._handleClick}/>
+                <Event
+                  event={event}
+                  key={event.id}
+                  selected={selectedEvent ? selectedEvent.id : null}
+                  handleClick={this._handleClick}
+                />
             )}
             </div>
           : <p>events loading</p>
